8eabc40687c9ff0b8510eefe3fd0ddf317b42fa6
Caen De Silva <caen@desilva.se>
2024-06-07 21:26:36 +0200

    Revert "Replace '?:' with if"
    This reverts commit 1067b5453236a7da256fac5ca288d5e49efcd2f4.

--- index.php
+++ index.php
@@ -87,15 +87,11 @@ class Main extends App
         try {
             return new Response(200, 'OK', $this->getResponseData());
         } catch (Throwable $exception) {
-            if (getenv('APP_DEBUG')) {
-                return new Response(500, 'Internal Server Error', [
-                    'error' => $exception->getMessage(),
-                ]);
-            } else {
-                return new Response(500, 'Internal Server Error', [
-                    'error' => 'Please see the server logs for more information.',
-                ]);
-            }
+            return new Response(500, 'Internal Server Error', [
+                'error' => getenv('APP_DEBUG') // If the `APP_DEBUG` environment variable is set, show the error message.
+                    ? $exception->getMessage()
+                    : 'Please see the server logs for more information.',
+            ]);
         }
     }
 
