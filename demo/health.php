cf7c12c9f9722579c195b8a2826abb5c95beed25
Caen De Silva <caen@desilva.se>
2024-06-14 22:47:20 +0200

    Create health.php

---
+++ demo/health.php
@@ -0,0 +1,51 @@
+{
+    "statusCode": 200,
+    "statusMessage": "OK",
+    "server_time": "2024-06-14 20:31:29 UTC (UTC)",
+    "server_name": "localhost",
+    "server_software": "PHP 8.1.2-1ubuntu2.17 Development Server",
+    "server_os_family": "Linux",
+    "server_os_version": "6.5.0-1021-azure",
+    "ping_time_ms": 1.56188011,
+    "execution_time_ms": 36.7410183,
+    "uptime": {
+        "days": 0,
+        "hours": 0,
+        "minutes": 2,
+        "human": "0 days, 0 hours, 2 minutes"
+    },
+    "load_average": {
+        "1min": 0.22509765625,
+        "5min": 0.12353515625,
+        "15min": 0.046875
+    },
+    "cpu": {
+        "brand": "AMD EPYC 7763 64-Core Processor",
+        "used": "5.50",
+        "idle": "94.50",
+        "core_speed_avg_mhz": "3,038.28",
+        "cores": 4,
+        "cpus": [
+            {
+                "core_id": "0",
+                "cpu_mhz": "3244.038",
+                "model_name": "AMD EPYC 7763 64-Core Processor"
+            },
+            {
+                "core_id": "1",
+                "cpu_mhz": "3124.189",
+                "model_name": "AMD EPYC 7763 64-Core Processor"
+            },
+            {
+                "core_id": "2",
+                "cpu_mhz": "2854.117",
+                "model_name": "AMD EPYC 7763 64-Core Processor"
+            },
+            {
+                "core_id": "3",
+                "cpu_mhz": "2930.767",
+                "model_name": "AMD EPYC 7763 64-Core Processor"
+            }
+        ]
+    }
+}
